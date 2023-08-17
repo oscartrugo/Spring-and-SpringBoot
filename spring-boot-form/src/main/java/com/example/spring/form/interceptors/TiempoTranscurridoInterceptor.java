866e842000af3020ba359a2a3004e3c378d65015
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-08-17 10:08:42 -0600

    98. Return en postHandle
    98. Return en postHandle

--- spring-boot-form/src/main/java/com/example/spring/form/interceptors/TiempoTranscurridoInterceptor.java
+++ spring-boot-form/src/main/java/com/example/spring/form/interceptors/TiempoTranscurridoInterceptor.java
@@ -19,33 +19,37 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        if(request.getMethod().equalsIgnoreCase("post")){
+        if (request.getMethod().equalsIgnoreCase("post")) {
             return true; //Se omite y no se aplica el código
         }
 
-        if(handler instanceof HandlerMethod){
+        if (handler instanceof HandlerMethod) {
             HandlerMethod metodo = (HandlerMethod) handler;
             logger.info("Es un método del controlador: " + metodo.getMethod().getName());
         }
 
         logger.info("TiempoTranscurridoInterceptor: preHandle() entrando ...");
-        logger.info("Interceptando ..." +  handler);
+        logger.info("Interceptando ..." + handler);
         long tiempoInicio = System.currentTimeMillis();
         request.setAttribute("tiempoInicio", tiempoInicio);
         Random random = new Random();
         Integer demora = random.nextInt(500); //Entre 0 y 500 (el valor introducido)
         Thread.sleep(demora);
+        
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
+        if (!request.getMethod().equalsIgnoreCase("post")) {
+            return;
+        }
         long tiempoInicio = (long) request.getAttribute("tiempoInicio");
         long tiempoFin = System.currentTimeMillis();
         long tiempoTranscurrido = tiempoFin - tiempoInicio;
 
-        if(handler instanceof HandlerMethod && modelAndView != null){
+        if (handler instanceof HandlerMethod && modelAndView != null) {
             modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
         }
 
