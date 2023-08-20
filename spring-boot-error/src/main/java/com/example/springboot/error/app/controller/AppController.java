78c0a530e91bef94dd40547bcd7985264ba95964
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-08-20 10:14:56 -0600

    108. Personalizando error 404 y 500

--- spring-boot-error/src/main/java/com/example/springboot/error/app/controller/AppController.java
+++ spring-boot-error/src/main/java/com/example/springboot/error/app/controller/AppController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
+    @SuppressWarnings("unused") //Suprime los warning de valores que no estén siendo utilizados
     @GetMapping("/index")
     public String index(){
+        Integer valor = 100/0;
         return "index";
     }
 }
