105d55b116f9d57b5482a9cb41804a8360427dd7
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-08-19 18:33:24 -0600

    102. Creando interceptor horario

---
+++ spring-boot-horario/src/main/java/com/example/springboot/horario/interceptors/HorarioInterceptor.java
@@ -0,0 +1,45 @@
+package com.example.springboot.horario.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Calendar;
+
+@Component
+public class HorarioInterceptor implements HandlerInterceptor {
+    @Value("${config.horario.apertura}")
+    private Integer apertura;
+    @Value("${config.horario.cierre}")
+    private Integer cierre;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        Calendar calendar = Calendar.getInstance();
+        int hora = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if(hora >= apertura && hora < cierre){
+            StringBuilder mensaje = new StringBuilder("Bienvenido al horario de atencion a clientes");
+            mensaje.append(", atendemos desde las ");
+            mensaje.append(apertura);
+            mensaje.append("hrs. ");
+            mensaje.append("Hasta las ");
+            mensaje.append(cierre);
+            mensaje.append(". Gracias por su visita");
+            request.setAttribute("mensaje", mensaje.toString());
+            return true;
+        }
+        response.sendRedirect(request.getContextPath().concat("/cerrado"));
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        String mensaje = (String) request.getAttribute("mensaje");
+        modelAndView.addObject("horario", mensaje); //Le mandamos el objeto a la vista en index.html
+    }
+}
