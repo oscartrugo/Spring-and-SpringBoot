e6680590d0a128837b31f2b852ded934aab4bdab
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-08-22 12:10:08 -0600

    119. Creando la clase de Acceso a Datos
    119. Creando la clase de Acceso a Datos o DAO con EntityManager - @Repository

---
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,23 @@
+package com.example.springboot.app.models.dao;
+
+import com.example.springboot.app.models.entity.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @SuppressWarnings("unchecked")
+    @Transactional(readOnly = true) //Marcamos al método como transactional sólo de lectura
+    @Override
+    public List<Cliente> findAll() {
+        return entityManager.createQuery("from Cliente").getResultList();
+    }
+}
---
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.example.springboot.app.models.dao;
+
+import com.example.springboot.app.models.entity.Cliente;
+
+import java.util.List;
+
+public interface IClienteDao {
+    public List<Cliente> findAll();
+
+}
