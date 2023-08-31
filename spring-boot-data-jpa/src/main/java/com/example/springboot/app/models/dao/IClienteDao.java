31586d76997a47177ad062129f28aa2be6e42e02
Oscar Trujillo <98614385+oscartrugo@users.noreply.github.com>
2023-08-31 15:10:32 -0600

    135. Implementando el Dao con la interface CrudRepository
    Uso de la interfaz CrudRepository

--- spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/dao/IClienteDao.java
@@ -1,13 +1,7 @@
 package com.example.springboot.app.models.dao;
 
 import com.example.springboot.app.models.entity.Cliente;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
-public interface IClienteDao {
-    public List<Cliente> findAll();
-    public void save(Cliente cliente);
-    public Cliente findOne(Long id);
-    public void delete(Long id);
-
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
 }
--- spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/service/ClienteServiceImpl.java
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/service/ClienteServiceImpl.java
@@ -17,7 +17,7 @@ public class ClienteServiceImpl implements IClienteService{
     @Override
     @Transactional(readOnly = true)
     public List<Cliente> findAll() {
-        return clienteDao.findAll();
+        return (List<Cliente>) clienteDao.findAll();
     }
 
     @Override
@@ -29,12 +29,12 @@ public class ClienteServiceImpl implements IClienteService{
     @Override
     @Transactional(readOnly = true) //Marcamos al método como transactional sólo de lectura
     public Cliente findOne(Long id) {
-        return clienteDao.findOne(id);
+        return clienteDao.findById(id).orElse(null);
     }
 
     @Override
     @Transactional
     public void delete(Long id) {
-        clienteDao.delete(id);
+        clienteDao.deleteById(id);
     }
 }
